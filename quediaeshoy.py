0814a791a2b1dc48d8d6f41ffe277bf81f9cd859
Juanmigomez <juanmiguel.gomez@gmail.com>
2017-01-17 11:05:33 +0100

    asdf

--- quediaeshoy.py
+++ quediaeshoy.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python
+import locale
+from datetime import datetime, date
+
+# Configuramos el locale a espanol
+locale.setlocale(locale.LC_TIME, "es_ES.utf8")
+
+# Asignamos a hoy la fecha actual
+hoy = date.today()
+
+# La imprimimos con formato
+print(hoy.strftime("Hola, hoy es %A %d de %B de %Y"))
 
